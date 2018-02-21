9b24dd1ce9ebbfdec6a855a37991303f30007342
dlewiswsol <33841159+dlewiswsol@users.noreply.github.com>
2018-02-21 09:02:37 -0600

    updates to the databsae project for the Harver changes

--- WSOL/WSOL/dbo/Tables/WSOL_TB_IMPORTS_HARVER_ASSESSMENTS.sql
+++ WSOL/WSOL/dbo/Tables/WSOL_TB_IMPORTS_HARVER_ASSESSMENTS.sql
@@ -1,18 +1,24 @@
 ﻿CREATE TABLE [dbo].[WSOL_TB_IMPORTS_HARVER_ASSESSMENTS] (
-    [ID_APPLICANT]           INT             NOT NULL,
-    [MATCHING_SCORE]         INT             NULL,
-    [DECISIVENESS]           INT             NULL,
-    [STRESS_TOLERANCE]       INT             NULL,
-    [PLANNING_ORGANISING]    INT             NULL,
-    [BEHAVIORAL_FLEXIBILITY] INT             NULL,
-    [ORAL_COMMUNICATION]     INT             NULL,
-    [LEADERSHIP]             INT             NULL,
-    [LEADERSHIP_SJT]         DECIMAL (10, 2) NULL,
-    [MULTITASKING]           DECIMAL (10, 2) NULL,
-    [MULTITASKING_SJT]       DECIMAL (10, 2) NULL,
-    [PERSONALITY_TEST]       DECIMAL (10, 2) NULL,
-    [LANGUAGE_TEST]          DECIMAL (10, 2) NULL,
-    [ID]                     INT             IDENTITY (1, 1) NOT NULL,
-    CONSTRAINT [PK_WSOL_TB_IMPORTS_HARVER_ASSESSMENTS] PRIMARY KEY CLUSTERED ([ID] ASC)
+    [IDAPPLICANT]                INT             NOT NULL,
+    [AGENTIDENTNUM]              VARCHAR (9)     NULL,
+    [OVERALL_TALENT_PITCH_SCORE] INT             NULL,
+    [DECISIVENESS]               INT             NULL,
+    [STRESS_TOLERANCE]           INT             NULL,
+    [PLANNING_ORGANISING]        INT             NULL,
+    [BEHAVIORAL_FLEXIBILITY]     INT             NULL,
+    [ORAL_COMMUNICATION]         INT             NULL,
+    [LEADERSHIP]                 INT             NULL,
+    [SJT_1]                      DECIMAL (10, 2) NULL,
+    [MULTITASKING]               DECIMAL (10, 2) NULL,
+    [SJT_2]                      DECIMAL (10, 2) NULL,
+    [PERSONALITY_TEST]           DECIMAL (10, 2) NULL,
+    [LANGUAGE_TEST]              DECIMAL (10, 2) NULL,
+    [WS_ROW_CREATED_TIME]        DATETIME        NULL,
+    [WS_ROW_UPDATED_TIME]        DATETIME        NULL,
+    [ID]                         INT             IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_WSOL_TB_IMPORTS_HARVER_ASSESSMENTS] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_APPLICANT_IDAPPLICANT] FOREIGN KEY ([IDAPPLICANT]) REFERENCES [dbo].[APPLICANT] ([IDAPPLICANT]) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
 
